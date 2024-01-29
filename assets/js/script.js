023d0cbe61d1412be65d3c9ee11b160f6c6e950d
SiminAnaraki <simin.anaraki@gmail.com>
2024-01-29 21:36:29 +0000

    fetching data from weather API

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,8 +6,9 @@ var oldInput
 var lat
 var lon
 var savedArray=[]
-init()
 
+
+init()
 
 function init(){
     var savedCity = JSON.parse(localStorage.getItem("city"))
@@ -25,9 +26,8 @@ function init(){
     }}
 
 
-
-
-searchBtn.addEventListener("click",search)
+searchBtn.addEventListener("click",search);
+$(".list-group").on("click","#newBtn",historySearch);
 
 function search(event){
     event.preventDefault()
@@ -37,12 +37,11 @@ function search(event){
     fetchData(userInput)
 }
 
-$(".list-group").on("click","#newBtn",oldSearch)
-    function oldSearch(event){
-        event.preventDefault()
-        oldInput = $(this).text()
-        fetchData(oldInput)
-    }
+function historySearch(event){
+    event.preventDefault()
+    oldInput = $(this).text()
+    fetchData(oldInput)
+}
 
 
 function fetchData(name){
@@ -70,13 +69,18 @@ function fetchData(name){
             console.log(data)
             var listResult = data.list
             var CityName = data.city.name
-            var city = $("<h3>").text(`${CityName} ${dayjs().format("DD/MM/YYYY")}`)
+            var city = $("<h3>").text(`${CityName} ${dayjs.unix(listResult[0].dt).format("DD/MM/YYYY")}`)
             var CTemp = (data.list[0].main.temp) - 273.15
             var temp = $("<p>").text(`Temp: ${CTemp.toFixed(2)} ºC `)
             var wind = $("<p>").text(`Wind: ${data.list[0].wind.speed} KMP`)
             var humidity = $("<p>").text(`Humidity: ${data.list[0].main.humidity}%`)
             $("#today").append(city,temp,wind,humidity)
-
+            for( var i = 0 ; i < listResult.length ; i++){
+                var day = $("<div>")
+                var time = $("<h3>").text(dayjs.unix(listResult[i].dt).format("DD/MM/YY"))
+                day.append(time)
+                $("#forecast").append(day)
+            }
     })
      searchInput.value=""
     })}}
